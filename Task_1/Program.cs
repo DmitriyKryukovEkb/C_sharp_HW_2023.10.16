cb84c80662ad7bcae0813fd52295f03b1dab7a80
DmitriyKryukovEkb <dmitriy_kryukov_ekb@mail.ru>
2023-10-17 19:58:30 +0500

    Task 1 correct version

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,15 +1,31 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
-int MyExponention (int inputNumber, int inputExp)
+int ReadInt(string msg) // объявление функции
 {
-    int result = 1;
-    for (int i = 0; i < inputExp; i++)
-    {
-        result *= inputNumber;
-    }
-    return result;
+    System.Console.WriteLine(msg); // вывод на экран параметра msg функции
+    string inputString = Console.ReadLine(); // считывает строку из консоли
+    int number = int.Parse(inputString); // преобразует строку в число
+    return number; // возврат значения
 }
-int a = 3;
-int b = 5;
-System.Console.WriteLine($"{a} в степени {b} = {MyExponention(3, 5)}");
+
+int MyExponention(int inputNumber, int inputExp)
+{
+    if (inputExp > 0)
+    {
+        int result = 1;
+        for (int i = 0; i < inputExp; i++)
+        {
+            result *= inputNumber;
+        }
+        return result;
+    }
+    else 
+    {
+        throw new InvalidOperationException("Нужна натуральная степень");
+    }
+}
+
+int inNumber = ReadInt("Введите целое число для возведения в степень");
+int inExp = ReadInt("Введите степень (только положительное целое число)");
+System.Console.WriteLine($"{inNumber} в степени {inExp} = {MyExponention(inNumber, inExp)}");
