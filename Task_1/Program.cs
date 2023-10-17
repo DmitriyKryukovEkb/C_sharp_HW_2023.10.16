cc7e38079450488ee979a55fd416c97625fed2e6
DmitriyKryukovEkb <dmitriy_kryukov_ekb@mail.ru>
2023-10-17 19:37:15 +0500

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+int MyExponention (int inputNumber, int inputExp)
+{
+    int result = 1;
+    for (int i = 0; i < inputExp; i++)
+    {
+        result *= inputNumber;
+    }
+    return result;
+}
+int a = 3;
+int b = 5;
+System.Console.WriteLine($"{a} в степени {b} = {MyExponention(3, 5)}");
