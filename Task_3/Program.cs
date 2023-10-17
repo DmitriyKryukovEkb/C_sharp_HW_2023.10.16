3a6ac43a8a8853a6666737f5be2bab020024b1e2
DmitriyKryukovEkb <dmitriy_kryukov_ekb@mail.ru>
2023-10-17 22:07:14 +0500

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] createMassive(int length, int min, int max) // создаем массив случайных чисел от min до max
+{
+    int[] massive = new int [length];
+    max += 1;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        massive[i] = new Random().Next(min, max);
+    }
+    return massive;
+}
+
+
+void printMassive(int[] massive)
+{
+    foreach (var item in massive) // цикл для работы с массивом
+    {
+        Console.Write(item + " ");
+    }
+}
+printMassive(createMassive(8, 0, 20));
